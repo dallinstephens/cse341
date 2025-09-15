979c3e9f792ae84c0d5a9b0e1d13e48d9ad8b9ef
DallinS <dallinstephens1@gmail.com>
2025-09-15 09:44:16 -0600

    lesson 1

---
+++ server.js
@@ -0,0 +1,17 @@
+// Resources for node and express tutorial:
+// https://www.youtube.com/watch?v=K00J87SofEc
+// https://codeforgeek.com/express-nodejs-tutorial/
+
+// express web server
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Corbin Stephens");
+});
+
+const localhostPort = 3000;
+
+app.listen(process.env.PORT || localhostPort, () => {
+    console.log('Web Server is listening at port ' + (process.env.PORT || 3000) + '.');
+})
