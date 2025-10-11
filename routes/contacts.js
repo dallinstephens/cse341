3031bd6f124e278fcbd3da147d4ae8261b3ea1f6
DallinS <dallinstephens1@gmail.com>
2025-10-11 16:42:32 -0600

    fixed route path issues

--- routes/contacts.js
+++ routes/contacts.js
@@ -3,16 +3,16 @@ const routes = require('express').Router();
 const contactsController = require('../controllers/contactsController');
 
 // routes.get('/', contactsController.contactsMenu);
-routes.get('/contacts/', contactsController.getAllContacts);
-routes.get('/contacts/:id', contactsController.getSingleContact);
+routes.get('/', contactsController.getAllContacts);
+routes.get('/:id', contactsController.getSingleContact);
 
-routes.post('/contacts', contactsController.createContact);
+routes.post('/', contactsController.createContact);
 
 // The contactId without the colon is static text in this: /contacts/contactId
 // The contactId with the colon is a dynamic variable in this: /contacts/:contactId
-routes.put('/contacts/:id', contactsController.updateContact);
+routes.put('/:id', contactsController.updateContact);
 
-routes.delete('/contacts/:id', contactsController.deleteContact);
+routes.delete('/:id', contactsController.deleteContact);
 
 // Reference for POST video:
 // https://youtu.be/vjf774RKrLc?si=J_Kf-zjWhTb2u5iK
